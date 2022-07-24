d435cc829b1ebecf592172b0f575342ccca4490d
anh vo <anhvth.226@gmail.com>
2022-07-24 05:53:07 +0000

    update

--- configs/00_mnist_vanila.py
+++ configs/00_mnist_vanila.py
@@ -1,5 +1,178 @@
+import pytorch_lightning as pl
+import timm, torch
+
+from lit_classifier.lit_model import *
+from lit_classifier.loss import FocalLoss
+from torch.utils.data import DataLoader, random_split
+
+# --------------------Lit model config
+
+#export
+
+from torch.optim.lr_scheduler import LambdaLR
+class MyLitModel(pl.LightningModule):
+    def __init__(self, model, optim_cfg=None, loss=FocalLoss()):
+        super().__init__()
+        self.model = model
+        self.loss_fn = loss
+        self.optim_cfg = optim_cfg
+        self.lr = 10e-3
+
+    def get_linear_schedule_with_warmup(self, optimizer, num_warmup_steps, num_training_steps, init_lr, min_lr, num_epochs, interval):
+        def lr_lambda(current_step: int):
+            if current_step < num_warmup_steps:
+                x = (1-init_lr)*(current_step / num_warmup_steps)+init_lr
+                return x
+            if interval=='epoch':
+                steps_per_ep = num_training_steps / num_epochs
+                current_ep = current_step // steps_per_ep
+                current_step = steps_per_ep*current_ep
+
+            total_step = (num_training_steps-num_warmup_steps)
+            current_step = current_step-num_warmup_steps
+            rt = min_lr+(1-min_lr)*(1-current_step/total_step)
+            return rt
+
+        return LambdaLR(optimizer, lr_lambda, -1)
+
+    def configure_optimizers(self):
+        if self.optim_cfg is None:
+            logger.warning('Please add optim cfg and re-init this object')
+            return
+        if self.optim_cfg['optim'] == 'Adam':
+            optimizer = torch.optim.Adam(self.parameters(), lr=self.optim_cfg["lr"])
+        elif self.optim_cfg['optim'] == 'AdamW':
+            optimizer = torch.optim.AdamW(self.parameters(), lr=self.optim_cfg["lr"], 
+                                          betas=(0.9, 0.999), eps=1e-08, weight_decay=0.01, amsgrad=False)
+        else:
+            assert isinstance(self.optim_cfg['optim'], torch.optim.Optimizer) 
+            optimizer = self.optim_cfg['optim']
+
+        scheduler = {
+            "scheduler": self.get_linear_schedule_with_warmup(
+                optimizer,
+                self.optim_cfg["steps"] * 0.15,
+                self.optim_cfg["steps"],
+                self.optim_cfg["init_lr"],
+                self.optim_cfg["min_lr"],
+                self.optim_cfg["epochs"],
+                self.optim_cfg['interval']
+            ),
+            "interval": 'step',  # or 'epoch'
+            "frequency": 1,
+        }
+
+        return [optimizer], [scheduler]
+
+    def forward(self, x):
+        return self.model(x)
+
+    def predict_step(self, batch, batch_idx):
+        x = batch
+        logits = self(x)
+        scores = logits.sigmoid()
+        # return dict(scores=scores)
+        return scores
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch[:2]
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+
+        preds = logits.softmax(1).argmax(1)
+        accs = (y == preds).float().mean()
+
+        self.log("val_loss",
+                 loss,
+                 rank_zero_only=True,
+                 prog_bar=True,
+                 on_step=False,
+                 on_epoch=True)
+        self.log("val_acc",
+                 accs,
+                 rank_zero_only=True,
+                 prog_bar=True,
+                 on_step=False,
+                 on_epoch=True)
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        # if batch_idx % 10 == 0:
+        #     print(batch_idx, self.trainer.optimizers[0])
+        #     print(batch_idx, self.lr)
+        #     print('-'*20)
+        x, y = batch[:2]
+        logits = self(x)
+        loss = self.loss_fn(logits, y)
+        preds = logits.softmax(1).argmax(1)
+        accs = (y == preds).float().mean()
+
+        self.log("training_loss",
+                 loss,
+                 prog_bar=True,
+                 rank_zero_only=True,
+                 on_epoch=True)
+        self.log("training_accuracy",
+                 accs,
+                 prog_bar=True,
+                 rank_zero_only=True,
+                 on_epoch=True)
+        return loss
+
+#-------------------------------- DATA CONFIG
+class ClassifierDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir: str = "./nbs/", batch_size: int = 32, num_workers:int = 4):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def setup(self, stage):
+        from timm.data.transforms_factory import (transforms_imagenet_eval,
+                                                  transforms_imagenet_train)
+        from torchvision import transforms
+        from torchvision.datasets import MNIST
+        
+
+
+        def to_rgb(x):
+            return x.convert('RGB')
+
+        train_transform = transforms.Compose([
+            transforms.Lambda(to_rgb),
+            *transforms_imagenet_train(32).transforms,
+        ])
+        self.mnist_test = MNIST(self.data_dir,
+                                train=False,
+                                transform=train_transform)
+        self.mnist_predict = MNIST(self.data_dir,
+                                   train=False,
+                                   transform=train_transform)
+        mnist_full = MNIST(self.data_dir,
+                           train=True,
+                           transform=train_transform)
+        self.mnist_train, self.mnist_val = random_split(
+            mnist_full, [55000, 5000])
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def predict_dataloader(self):
+        return DataLoader(self.mnist_predict, batch_size=self.batch_size, num_workers=self.num_workers)
+
+
 class Exp:
-    def get_model():
-        pass
-    def get_data():
-        pass
+    def get_model(self):
+        model = timm.create_model('resnet18', True, num_classes=10)
+        opt = get_optim_cfg(10, 55000//32)
+        return MyLitModel(model, opt)
+
+    def get_data(self):
+        return ClassifierDataModule()
--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
-from pytorch_lightning import Trainer, seed_everything
+
 import mmcv
+import os
+import os.path as osp
 
 def get_exp_by_file(exp_file):
     """
@@ -9,25 +11,77 @@ def get_exp_by_file(exp_file):
     try:
         import sys, os, importlib
         sys.path.append(os.path.dirname(exp_file))
+        # import ipdb; ipdb.set_trace()
         current_exp = importlib.import_module(os.path.basename(exp_file).split(".")[0])
+        current_exp = importlib.reload(current_exp)
         exp = current_exp.Exp()
+        return exp
     except Exception:
         raise ImportError("{} doesn't contains class named 'Exp'".format(exp_file))
-    return exp
 
 
-def main(args):
-    cfg = get_exp_by_file(args.cfg)
-    import ipdb; ipdb.set_trace()
+def get_trainer(exp_name, gpus=1, max_epochs=40, distributed=False,
+        monitor=dict(metric="val_acc", mode="max"), save_every_n_epochs=1, save_top_k=1, tqdm_refesh_rate=10,
+    trainer_kwargs=dict()):
+    from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+    from datetime import datetime, timedelta
+    from pytorch_lightning.callbacks.progress import TQDMProgressBar
+    from pytorch_lightning.loggers import TensorBoardLogger
+    from pytorch_lightning import Trainer, seed_everything
+
+    now = datetime.now() + timedelta(hours=7)
+    
+    root_log_dir = osp.join(
+            "lightning_logs", exp_name)
+    cur_num_exps = len(os.listdir(root_log_dir)) if osp.exists(root_log_dir) else 0
+    version = now.strftime(f"{cur_num_exps:02d}_%b%d_%H_%M")
+    root_log_dir = osp.join(root_log_dir, version)
+
+    filename="{epoch}-{"+monitor["metric"]+":.2f}"
+
+    callback_ckpt = ModelCheckpoint(
+        dirpath=osp.join(root_log_dir, "ckpts"),
+        monitor=monitor['metric'],mode=monitor['mode'],
+        filename=filename,
+        save_last=True,
+        every_n_epochs=save_every_n_epochs,
+        save_top_k=save_top_k,
+    )
+
+    callback_tqdm = TQDMProgressBar(refresh_rate=tqdm_refesh_rate)
+    callback_lrmornitor = LearningRateMonitor(logging_interval="step")
+    plt_logger = TensorBoardLogger(
+        osp.join(root_log_dir, "tb_logs"), version=version
+    )
+    
+    trainer = Trainer(
+        gpus=gpus,
+        max_epochs=max_epochs,
+        strategy= "dp" if not distributed else "ddp",
+        callbacks=[callback_ckpt, callback_tqdm, callback_lrmornitor],
+        logger=plt_logger,**trainer_kwargs,
+    )
+    return trainer
+
+def main(params):
+    cfg = get_exp_by_file(params.cfg)
+    exp_name = osp.basename(params.cfg).split('.')[0]
     model = cfg.get_model()
     data = cfg.get_data()
-    trainer = Trainer.from_argparse_args(args)
+    
+    trainer = get_trainer(exp_name)
+    
+    if params.verbose:
+        print(params.pretty_text)
     trainer.fit(model, data)
 
 if __name__ == '__main__':
     seed_everything(0, workers=True)
     parser = ArgumentParser()
     parser.add_argument('cfg')
-    parser = Trainer.add_argparse_args(parser)
-    args = parser.parse_args()
-    main(args)
+    parser.add_argument('--verbose', action='store_true')
+    # parser = Trainer.add_argparse_args(parser)
+    
+    params = mmcv.Config(parser.parse_args().__dict__)
+
+    main(params)
