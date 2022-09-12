4d203aaa4c24fbb09257406c3dac9a9007db34e4
Võ Trần Hải Anh <v.anhvth8@vinai.io>
2022-09-12 02:29:04 +0000

    update

--- ple/all.py
+++ ple/all.py
@@ -2,6 +2,6 @@ from ple.lit_model import *
 try:
     from ple.loss import *
 except Exception as e:
-    print(f'{e} while import loss')
+    print(f'{e} while import loss, consider this command "pip install mmcv-full"')
 from ple.base_exp import *
 from ple.trainer import *
