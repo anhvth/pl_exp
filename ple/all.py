85d85f19ffc3012423c997cdb0f54ff887b85c43
Võ Trần Hải Anh <v.anhvth8@vinai.io>
2022-10-28 05:15:27 +0000

    add ple datasets

--- ple/all.py
+++ ple/all.py
@@ -5,3 +5,4 @@ except Exception as e:
     print(f'{e} while import loss, consider this command "pip install mmcv-full"')
 from ple.base_exp import *
 from ple.trainer import *
+from ple.datasets import *
