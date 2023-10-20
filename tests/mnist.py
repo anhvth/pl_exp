e21ae1e90615d5ab3ab1095cc1ef8586ccd1c5ab
anh vo <anhvth.226@gmail.com>
2023-10-20 18:37:05 +0000

    update

--- tests/mnist.py
+++ tests/mnist.py
@@ -25,8 +25,8 @@ train_length = int(0.99 * len(train_ds))
 val_length = len(train_ds) - train_length
 train_ds, val_ds = torch.utils.data.random_split(train_ds, [train_length, val_length])
 
-dl_train = torch.utils.data.DataLoader(val_ds, BZ, num_workers=NUM_WORKERS, shuffle=True)
-dl_val = torch.utils.data.DataLoader(val_ds, BZ, num_workers=NUM_WORKERS, shuffle=False)
+DL_TRAIN = torch.utils.data.DataLoader(val_ds, BZ, num_workers=NUM_WORKERS, shuffle=True)
+DL_VAL = torch.utils.data.DataLoader(val_ds, BZ, num_workers=NUM_WORKERS, shuffle=False)
 
 # ---- MODEL DEFINITION ----
 class SimpleCNN(torch.nn.Module):
@@ -39,8 +39,7 @@ class SimpleCNN(torch.nn.Module):
         )
         
     def forward(self, x):
-        x = x.flatten(1)
-        x = self.layers(x)
+        x = self.layers(x.flatten(1))
         return x
 
 model = SimpleCNN()
@@ -49,38 +48,30 @@ model = SimpleCNN()
 optim = lambda params: torch.optim.Adam(params, lr=LR)
 
 
-# sched = _lr_function_by_epoch(
-#     num_epochs=EPOCHS,
-#     num_steps_per_epoch=len(dl_train),
-#     num_epochs_per_cycle=EPOCHS // 2,
-#     min_lr=1/100,
-#     cycle_decay=0.7
-# )
 
 import pytorch_lightning as pl
 import torch.nn as nn
 import torch
 
-class LitModel(AbstractLitModel):
+class CustomLitModel(AbstractLitModel):
+
     def validation_step(self, batch, batch_idx):
-        
         pred = self(batch[0])
         loss = self.loss_fn(pred, batch[1])
         self.log('val/loss', loss, prog_bar=True)
+
     def training_step(self, batch, batch_idx):
-        print(self.global_step)
         pred = self(batch[0])
         loss = self.loss_fn(pred, batch[1])
-        self.log('train/loss', loss, prog_bar=True, )
+        self.log('train/loss', loss, prog_bar=True)
         return loss
-# print(self.global_step)
 
 # Usage
-lit = LitModel(
+lit = CustomLitModel(
     model=model,
-    lr=1e-5, # You can specify this if you want to override the default
+    lr=LR,
     loss_fn=nn.CrossEntropyLoss(),
-    train_loader=dl_train,
+    train_loader=DL_TRAIN,
     num_epochs=EPOCHS, 
     grad_accumulate_steps=GRAD_ACCUMULATE_STEPS,
     optim = torch.optim.Adam(model.parameters(), 1e-5)
@@ -88,9 +79,13 @@ lit = LitModel(
 
 # ---- TRAINING ----
 trainer = get_trainer(
-    EXP_NAME, EPOCHS, num_gpus=GPUS, overfit_batches=0.0,
-    monitor={'metric': 'val/loss', 'mode': 'min'}, 
-    strategy=STRATEGY, 
+    name=EXP_NAME,
+    max_epochs=EPOCHS,
+    num_gpus=GPUS,
+    overfit_batches=0.0, # You can adjust this if needed
+    monitor={'metric': 'val/loss', 'mode': 'min'},
+    strategy=STRATEGY,
 )
+
 # import ipdb; ipdb.set_trace()
-trainer.fit(lit, dl_train, dl_val)
+trainer.fit(lit, DL_TRAIN, DL_VAL)
